6ba3d7c3a67b7952f1347321237e15463e8305c5
jas07061002 <jas18kalra@gmail.com>
2023-01-23 18:11:49 -0500

    git commit

---
+++ Lecture3/src/test/java/com/sheridancollege/demo/Lecture3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheridancollege.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
